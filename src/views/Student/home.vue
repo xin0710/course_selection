73990d133939aeeebcc6c34041c8c9aae36c7e25
xin0710 <xin.wxh@qq.com>
2023-07-06 23:08:59 +0800

    基于vue+springboot的学生选课系统

--- src/views/Student/home.vue
+++ src/views/Student/home.vue
@@ -4,13 +4,7 @@
       <el-main>
         <h1>student home</h1>
         <info-card></info-card>
-        <el-card style="width: 40%">
-          <p><i class="el-icon-s-home" style="margin-right: 18px"></i>项目名：学生选课管理系统</p>
 
-
-          <p><i class="el-icon-chat-round" style="margin-right: 18px"></i>源码来自互联网版权归原作者所有</p>
-          <p><i class="el-icon-error" style="margin-right: 18px;"></i>仅供学习，禁止抄袭</p>
-        </el-card>
         <logout></logout>
       </el-main>
     </el-container>
